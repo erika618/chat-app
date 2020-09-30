1b0e32e4951d00c86d6eca18e636fa13ea355f8f
erika618 <68714247+erika618@users.noreply.github.com>
2020-09-30 22:28:22 +0900

    ログアウト機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "messages#index"
-  resources :users, only: [:edit, :update]
+  resources :users, only: [:edit, :update,]
+  # userのアクションがこの二つなのは後付けしたのがこの二つだから（それ以外はデバイスないで処理）
 end

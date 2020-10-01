c4bb67bc82a0eeb4060e034c95c14c9406bbb550
erika618 <68714247+erika618@users.noreply.github.com>
2020-10-01 20:30:41 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   root to: "messages#index"
   resources :users, only: [:edit, :update,]
   # userのアクションがこの二つなのは後付けしたのがこの二つだから（それ以外はデバイスないで処理）
+  resources :rooms, only: [:new, :create]
 end

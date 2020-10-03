49e6023a5e8520b46ab14ee7129a2e42e0becd25
erika618 <68714247+erika618@users.noreply.github.com>
2020-10-03 21:11:32 +0900

    画面遷移の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "messages#index"
+  root to: "rooms#index"
   resources :users, only: [:edit, :update,]
   # userのアクションがこの二つなのは後付けしたのがこの二つだから（それ以外はデバイスないで処理）
   resources :rooms, only: [:new, :create]

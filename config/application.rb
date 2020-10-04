de71c20b87b7bcc89988285782cf685bb2e03feb
erika618 <68714247+erika618@users.noreply.github.com>
2020-10-04 18:55:38 +0900

    投稿時刻の日本時間への修正

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,4 @@
+# このファイルには、そのRailsアプリケーションの開発環境すべてにおいて共通となる設定を記述
 require_relative 'boot'
 
 require 'rails/all'
@@ -10,6 +11,9 @@ module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo'
+    # 日本時間に設定
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
